21513fb4f7ea23ba5a0a46e3f11688459a0d8050
Shalu <shalini.kuzhippat@uconn.edu>
2023-11-26 18:14:57 -0500

    connect with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,23 @@ import 'dart:io';
 import 'package:firebase_1/screens/start_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_file_downloader/flutter_file_downloader.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyA6iI-DWru6OylA58srsbJoTd4dCTphWOQ",
+          appId: "1:435700051337:android:2e6566fc000e74f93d8558",
+          messagingSenderId: "435700051337",
+          projectId: "flutter-firebase-app-95c01"));
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  final String _url_ppt = 'https://www.africau.edu/images/default/sample.pdf';
+  final String _url_ppt =
+      'https://brand.uconn.edu/wp-content/uploads/sites/2820/2019/08/white-oakleaf-standard-temp.pptx';
 
   //const MyApp({super.key});
 
@@ -40,7 +49,7 @@ class MyApp extends StatelessWidget {
                 });
           },
           label: Text('Download'),
-          icon: Icon(Icons.edit),
+          icon: Icon(Icons.arrow_downward_rounded),
         ),
       ),
     );
